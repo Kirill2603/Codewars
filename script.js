2c4d59dae9d6b4dc58592b8d1e719c8955f7f0ea
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-05 17:06:45 +0300

    Convert boolean values to strings 'Yes' or 'No'.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function usdcny(usd) {
-  return `${usd * 6.75} Chinese Yuan`;
+function boolToWord(bool) {
+  if (bool == true) {
+    return "Yes";
+  } else if (bool == false) return "No";
 }
 
-console.log(usdcny(15));
+console.log(boolToWord(false));
