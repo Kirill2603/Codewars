55e24f67e110f35792f56841ac0367d3dab7d9cb
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-05 22:49:44 +0300

    Keep Hydrated!

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-var summation = function (num) {
-  let sum = 0;
-  for (let i = 0; i <= num; i++) {
-    sum += i;
-  }
-  return sum;
-};
+function litres(time) {
+  return Math.floor((time*0.5));
+}
 
-console.log(summation(8));
+console.log(litres(1787));
