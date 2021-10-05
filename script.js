dd6ab932c7104a9c92788513ecc0ebadd840e01a
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-05 14:35:28 +0300

    Highest and Lowest

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("BATMAN!");
+function highAndLow(numbers) {
+  let arr = numbers.split(" ");
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  return `${max} ${min}`;
+}
+
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
