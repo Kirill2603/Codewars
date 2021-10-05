23d715b3a4b75cec17c947563ba7c6b3e2dab85e
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-05 15:53:24 +0300

    USD => CNY

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-function checkExam(array1, array2) {
-  let sum = 0;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      sum += 4;
-    } else if (array2[i] == "") {
-      sum = sum;
-    } else sum -= 1;
-  }
-  if (sum < 0) return 0;
-  else return sum;
+function usdcny(usd) {
+  return `${usd * 6.75} Chinese Yuan`;
 }
 
-console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]));
+console.log(usdcny(15));
