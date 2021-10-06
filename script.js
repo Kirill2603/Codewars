625bf245387148346c028a677f736128cbe75de3
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-06 17:05:20 +0300

    Beginner - Lost Without a Map

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-var haystack_1 = [
-  "3",
-  "123124234",
-  undefined,
-  "needle",
-  "world",
-  "hay",
-  2,
-  "3",
-  true,
-  false,
-];
-
-function findNeedle(haystack) {
-  for (let i = 0; i < haystack.length; i++) {
-    if (haystack[i] == "needle") {
-      return `found the needle at position ${i}`;
-    }
-  }
+function maps(x) {
+  return x.map((x) => x * 2);
 }
 
-console.log(findNeedle(haystack_1));
+console.log(maps([1, 2, 3]));
