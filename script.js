9084f00ac0e4e936741c1751f97ea832f7e1d668
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-06 15:41:16 +0300

    Convert a String to a Number!

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-function repeatStr(n, str) {
-  var s = "";
-  for (var i = 0; i < n; i++) {
-    s += str;
-  }
-  return s;
-}
-
-console.log(repeatStr(3, "*"));
-=======
-function squareArea(A){
-  let Res = Number(((2 * A / 3.1416)**2).toFixed(2))
-  return Res
-}
-console.log(squareArea(2));
->>>>>>> c8230f829edafa09fcf2f33e4e2e2ceaf1a9fc83
+var stringToNumber = function (str) {
+  return Number(str);
+};
