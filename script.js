9944f5d1e3a4ef24cc6c57b33eb741a72c0830bd
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-06 17:46:46 +0300

    Count of positives / sum of negatives

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function invert(array) {
-  return array.map((x) => x * -1);
+let testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+
+function countPositivesSumNegatives(input) {
+  posCol = 0;
+  negSum = 0;
+  for (let i = 0; i < input.length; i++)
+    if (input[i] > 0) {
+      posCol++;
+    } else {
+      console.log((negSum += input[i]));
+    }
+  return [posCol, negSum];
 }
-console.log(reverseWords("hello world!"));
+
+console.log(countPositivesSumNegatives(testData));
