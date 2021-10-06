71b87f95955f6f910f802d99517015dcd75c85ba
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-06 15:56:57 +0300

    Convert number to reversed array of digits

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-var stringToNumber = function (str) {
-  return Number(str);
-};
+function digitize(n) {
+  let arr = [];
+  arr = String(n).split("").reverse().map(Number);
+  return arr;
+}
+console.log(digitize(123123124));
