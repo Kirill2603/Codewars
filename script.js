bd02915e9f1d1ce1f9522c0ea736e0d7294ff453
Kirill <64753246+Kirill2603@users.noreply.github.com>
2021-10-07 21:48:54 +0300

    SpeedCode #2 - Array Madness

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function squareArea(A){
-  let Res = Number(((2 * A / 3.1416)**2).toFixed(2))
-  return Res
+function arrayMadness(a, b) {
+  let sumA = 0
+  let sumB = 0
+  for (let i = 0; i < a.length; i++) {
+    sumA += a[i]**2
+  }
+  for (let i = 0; i < b.length; i++) {
+    sumB += b[i]**3
+  }
+
+  if (sumA>sumB) return true
+  else return false
 }
-console.log(squareArea(2));
+console.log(arrayMadness([1,3,5,2,4],[2,2,2,2,2,2,2,1]))
+
+
+// function arrayMadness(a, b) {
+//   return a.reduce( (sum, el) => sum + el ** 2, 0) >
+//          b.reduce( (sum, el) => sum + el ** 3, 0);
+// }
